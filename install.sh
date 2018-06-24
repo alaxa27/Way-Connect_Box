6145fed4db29d1178f4adeda259fa6366d87aeb2
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-24 22:35:41 +0200

    ln -s after file copied.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 apt-get update
 apt-get upgrade
-apt-get install -y hostapd dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx
+apt-get install -Y hostapd dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx
 
 cd nodogsplash/
 make && make install
 cd ../
 
-ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
 cp -R config/* /etc/
+ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
