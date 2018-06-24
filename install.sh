15b465a225f9b3939cf81916a5c0650726813635
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-24 22:25:21 +0200

    Add sysctl + nodogsplash conf

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-apt-get install -y hostapd dnsmasq python3 python3-dev python3-pip nginx
+apt-get update
+apt-get upgrade
+apt-get install -y hostapd dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx
+
+cd nodogsplash/
+make && make install
+cd ../
 
 ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
 cp -R config/* /etc/
