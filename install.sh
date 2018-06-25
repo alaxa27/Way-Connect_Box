458c38a2c2c35d10188e53e84489cbe7229b4701
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-25 14:41:50 +0200

    Config update

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 apt-get update
 apt-get upgrade
-apt-get install -Y hostapd dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx
+apt-get install -Y hostapd dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx-common nginx
 
 cd nodogsplash/
 make && make install
 cd ../
 
+cd middleware/
+pip3 install -r requirements.txt
+cd ../
+
 cp -R config/* /etc/
-ln -s /etc/nginx/sites-available/middleware /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/middleware.conf /etc/nginx/sites-enabled
