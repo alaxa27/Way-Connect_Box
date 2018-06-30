807059e428fdb84172188dddd8e412a31dae9ec4
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-30 16:34:00 +0200

    patch fix 2.

--- patch.sh
+++ patch.sh
@@ -2,6 +2,6 @@
 
 echo "Applying patch..."
 crontab -l > /home/pi/cron
-echo "@reboot /home/pi/Way-Connect_Box/tools/upgrade.sh" >> cron
+echo "@reboot /home/pi/Way-Connect_Box/tools/upgrade.sh" >> /home/pi/cron
 crontab /home/pi/cron
 rm /home/pi/cron
