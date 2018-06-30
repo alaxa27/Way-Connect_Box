f73f2ec8147f99ff1560ef0ca278131fc18292f1
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-30 16:57:33 +0200

    patch system upgrade.

--- patches/patch_0.sh
+++ patches/patch_0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Applying patch..."
+echo "Applying patch 0..."
 crontab -l > /home/pi/cron
 echo "@reboot /home/pi/Way-Connect_Box/tools/upgrade.sh" >> /home/pi/cron
 crontab /home/pi/cron
--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -8,14 +8,21 @@ if mkdir "$ROOTDIR/update.lock" 2>/dev/null; then
 
     . ${ROOTDIR}/way-box-update
     CURRENT_VERSION=$VERSION
+    CURRENT_PATCH=$PATCH
     . ${ROOTDIR}/way-box-update.remote
     REMOTE_VERSION=$VERSION
+    REMOTE_PATCH=$PATCH
 
     if [ $REMOTE_VERSION -gt $CURRENT_VERSION ]; then
       # Install the upgrade
       cd "$ROOTDIR/Way-Connect_Box"
       git pull --rebase
-      sh -c "$ROOTDIR/Way-Connect_Box/patch.sh"
+      count=$CURRENT_PATCH
+      while [ $count -lt $REMOTE_PATCH ]
+      do
+        sh -c "$ROOTDIR/Way-Connect_Box/patches/patch_$count.sh"
+        ((count++))
+      done
       rm $ROOTDIR/way-box-update
       mv $ROOTDIR/way-box-update.remote $ROOTDIR/way-box-update
       rmdir $ROOTDIR/update.lock
