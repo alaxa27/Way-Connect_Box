af2604a0e8616d96eaf8fb6bc43ad2462a199b50
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-30 06:11:09 +0200

    remove useless rm

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -19,7 +19,6 @@ if mkdir "$ROOTDIR/update.lock" 2>/dev/null; then
       rm $ROOTDIR/way-box-update
       mv $ROOTDIR/way-box-update.remote $ROOTDIR/way-box-update
     fi
-    rm $ROOTDIR/way-box-update.remote
   fi
   rmdir $ROOTDIR/update.lock
   reboot
