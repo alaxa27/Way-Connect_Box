23a52e4a90d5a081d5eb7c061420d8e445d2dd9f
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-02 07:22:21 +0200

    wc.com setup for redirection to fidelity portal

--- install.sh
+++ install.sh
@@ -11,10 +11,13 @@ pip3 install -r requirements.txt
 cd ../
 
 cp -R config/* /etc/
+cp -R wc.com/ /var/www/
 ln -s /etc/nginx/sites-available/middleware.conf /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/wc.com.conf /etc/nginx/sites-enabled
 systemctl daemon-reload
 systemctl enable middleware
 
+# Cronjob that checks for upgrades
 echo "0 5 * * * /home/pi/Way-Connect_Box/tools/upgrade.sh" >> cron
 crontab cron
 rm cron
---
+++ patches/patch_2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Applying patch 2..."
+
+cp /home/pi/Way-Connect_Box/config/hosts /etc/
+cp -R /home/pi/Way-Connect_Box/config/nginx /etc/
+cp -R /home/pi/Way-Connect_Box/wc.com/ /var/www/
+ln -s /etc/nginx/sites-available/wc.com.conf /etc/nginx/sites-enabled
