20e911f9ffbe8757169161af6a9a62cebcf98d9e
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-19 15:46:35 +0200

    patch_6 fix

--- patches/patch_6.sh
+++ patches/patch_6.sh
@@ -2,7 +2,5 @@
 
 echo "Applying patch 6..."
 
-echo "AP_MODE=\"wlan\"" > /home/pi/env
-
 cp /home/pi/Way-Connect_Box/patches/patch_6/config.txt /boot/
 cp /home/pi/Way-Connect_Box/patches/patch_6/rc.local /etc/
