95708fe65760e812e9e90ca3a4a878a4e599133c
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-27 21:57:01 +0200

    fix pi-bluetooth dpkg error.

--- patches/patch_8.sh
+++ patches/patch_8.sh
@@ -2,5 +2,7 @@
 
 echo "Applying patch 8..."
 
-cp /home/pi/Way-Connect_Box/patches/patch_8/hostapd.conf /etc/hostapd/
+rm /var/lib/dpkg/info/pi-bluetooth*
+apt-get install --reinstall pi-bluetooth
 apt-get install -y jq
+cp /home/pi/Way-Connect_Box/patches/patch_8/hostapd.conf /etc/hostapd/
