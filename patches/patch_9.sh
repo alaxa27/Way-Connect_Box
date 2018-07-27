c7e06b678b78662d73988f64ff2475c39ea9b58f
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-27 20:28:42 +0200

    .

--- patches/patch_9.sh
+++ patches/patch_9.sh
@@ -7,6 +7,12 @@ ROOTDIR="/home/pi"
 . ${ROOTDIR}/env
 NEXT_MODE=$AP_MODE
 
+if [ -z "$NEXT_MODE" ]
+then
+else
+  NEXT_MODE="wlan"
+fi
+
 ln -sf ${ROOTDIR}/Way-Connect_Box/config/iptables.${NEXT_MODE}.ipv4.nat /etc/iptables.ipv4.nat
 ln -sf ${ROOTDIR}/Way-Connect_Box/config/dnsmasq.${NEXT_MODE}.conf /etc/dnsmasq.conf
 ln -sf ${ROOTDIR}/Way-Connect_Box/config/rc.${NEXT_MODE}.local /etc/rc.local
