92142e83f1aaf4540382752a5f10220d4e587cf5
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-27 20:31:44 +0200

    patch_9 fix.

--- patches/patch_9.sh
+++ patches/patch_9.sh
@@ -9,8 +9,9 @@ NEXT_MODE=$AP_MODE
 
 if [ -z "$NEXT_MODE" ]
 then
-else
   NEXT_MODE="wlan"
+else
+  echo "AP mode configured already."
 fi
 
 ln -sf ${ROOTDIR}/Way-Connect_Box/config/iptables.${NEXT_MODE}.ipv4.nat /etc/iptables.ipv4.nat
