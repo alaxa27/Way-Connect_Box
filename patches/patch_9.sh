26590b18ec761d4898bdc3c9e70aab08412e4fc8
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-27 19:38:53 +0200

    Apply ln as patch.

---
+++ patches/patch_9.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Applying patch 9..."
+
+ROOTDIR="/home/pi"
+
+. ${ROOTDIR}/env
+NEXT_MODE=$AP_MODE
+
+ln -sf ${ROOTDIR}/Way-Connect_Box/config/iptables.${NEXT_MODE}.ipv4.nat /etc/iptables.ipv4.nat
+ln -sf ${ROOTDIR}/Way-Connect_Box/config/dnsmasq.${NEXT_MODE}.conf /etc/dnsmasq.conf
+ln -sf ${ROOTDIR}/Way-Connect_Box/config/rc.${NEXT_MODE}.local /etc/rc.local
+ln -sf ${ROOTDIR}/Way-Connect_Box/config/network/interfaces.${NEXT_MODE} /etc/network/interfaces
+ln -sf ${ROOTDIR}/Way-Connect_Box/config/nodogsplash/nodogsplash.${NEXT_MODE}.conf
