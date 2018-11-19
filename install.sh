e169584f6fe064a90a46ea87f21b0a7a0ea8987a
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-19 12:52:47 +0100

    migrate to python 3.7 stack

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 apt update
 apt -y upgrade
-apt install -y hostapd ipset dnsmasq libmicrohttpd-dev python3 python3-dev python3-pip nginx-common nginx
+apt install -y hostapd ipset dnsmasq libmicrohttpd-dev nginx-common nginx
+apt install -y  build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
+
+cd ../
+wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tar.xz
+tar xf Python-3.7.1.tar.xz
+cd Python-3.7.1
+./configure
+make -j 4
+sudo make altinstall
+cd ../
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.7 get-pip.py
+cd Way-Connect_Box
 
 cd nodogsplash/
 make && make install
@@ -27,3 +40,5 @@ cp keys ../
 
 git config --global user.email "a@a.a"
 git config --global user.name "a"
+
+
--- middleware/recurrent_tasks.py
+++ middleware/recurrent_tasks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 from dotenv import load_dotenv, main
 import fileinput
 import os
