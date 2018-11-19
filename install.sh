caedf484650a207d0c2e067ecfbc69f9d4d4883b
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-19 13:33:07 +0100

    linting

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 apt update
 apt -y upgrade
 apt install -y hostapd ipset dnsmasq libmicrohttpd-dev nginx-common nginx
-apt install -y  build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
+apt install -y  build-essential tk-dev libncurses5-dev libncursesw5-dev\
+    libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev\
+    libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev 
 
 cd ../
 wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tar.xz
