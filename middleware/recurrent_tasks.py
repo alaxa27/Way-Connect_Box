bf5cfd2bfa2928e642b938406b3e7f1d12b97bb0
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-11-20 12:08:13 +0100

    git update system PrototypeSkeleton

--- middleware/recurrent_tasks.py
+++ middleware/recurrent_tasks.py
@@ -8,6 +8,8 @@ import subprocess
 
 from sign import sign
 
+homePath = '/home/pi'
+
 configFilesLocations = {
     'INTERFACE_IN': [
         '/etc/iptables.ipv4.nat',
@@ -40,12 +42,47 @@ class MissingKeyInConfig(Exception):
     pass
 
 
+class UpdateFetchingError(Exception):
+    pass
+
+
+class BranchDoesNotExist(Exception):
+    pass
+
+
+class ApplyCommitError(Exception):
+    pass
+
+
+class ApplySameCommitException(Exception):
+    pass
+
+
+class PutVersionError(Exception):
+    pass
+
+
+def check_branch_exist(repo, branch):
+    pass
+
+
+def fetch_repo(repo):
+    pass
+    
+
+def get_latest_commit(repo, branch):
+    pass
+
+
+def apply_commit(repo, commitHash):
+    pass
+
+
 def get_config_key(config, key):
     try:
         value = config[key]
     except KeyError as key:
-        raise MissingKeyInConfig()
-
+        raise MissingKeyInConfig(f'{key} key is missing in configuration.')
     return value
 
 def replace_occurences(key, value, fileLocation):
@@ -54,34 +91,33 @@ def replace_occurences(key, value, fileLocation):
             print(line.replace(f'WC_{key}', value), end='')
 
 
-def get_current_config():
-    path = '/home/pi/env'
-    return main.dotenv_values(path)
+def get_current_config(configPath):
+    return main.dotenv_values(configPath)
 
     
-def copy_default_config():
-    subprocess.call('cp -R /home/pi/Way-Connect_Box/config/* /etc/', shell=True)
+def copy_default_config(fromDir, toDir):
+    subprocess.call(f'cp -R {fromDir}/* {toDir}/', shell=True)
     
 
 def reboot():
     subprocess.call('/sbin/shutdown -r now', shell=True)
 
 
-def save_config(config):
+def save_config(config, configPath):
     try:
-        with open('/home/pi/env', 'w') as file:
+        with open(configPath, 'w') as file:
             for key, value in config.items():
                 file.write(f'{key}="{value}"\n')
     except Exception as e:
         raise UnableToWriteConfig()
 
 
-def apply_config(config):
-    for var in configFilesLocations:
+def apply_config(config, configFiles):
+    for var in configFiles:
         if var not in config:
             raise MissingConfigOnServer()
 
-    for varName, fileLocations in configFilesLocations.items():
+    for varName, fileLocations in configFiles.items():
         for fileLocation in fileLocations:
             replace_occurences(varName, config[varName], fileLocation)
 
@@ -102,7 +138,10 @@ def post_box_status(
     boxStatus['update_message'] = update_message
     boxStatus['connected_customers'] = 0
 
-    res = requests.post(url='http://localhost:5000/portal/boxes/status/', json=boxStatus)
+    res = requests.post(
+        url='http://localhost:5000/portal/boxes/status/',
+        json=boxStatus
+        )
 
 
 def get_box_config():
@@ -133,20 +172,77 @@ def get_box_config():
     return response
 
 
-if __name__=='__main__':
-    post_box_status(True)
+def put_box_version(commitHash):
+    boxVersion = {}
+    boxVersion['commit'] = commitHash
+    
+    try:
+        res = requests.put(
+            url='http://localhost:5000/portal/boxes/version/',
+            json=boxVersion
+            )
+    except Exception:
+        raise PutVersionError('Error while putting the version to the backend.')
 
-    currentConfig = get_current_config()
+
+if __name__=='__main__':
+    envPath = f'{homePath}/env'
+    repoPath = f'{homePath}/Way-Connect_Box'
+    configDir = f'{repoPath}/config'
+    etcDir = '/etc'
+
+    currentConfig = get_current_config(envPath)
     remoteConfig = get_box_config()
 
     if remoteConfig != currentConfig:
-        copy_default_config()
+        copy_default_config(configDir, etcDir)
         try:
-            apply_config(remoteConfig)
+            apply_config(remoteConfig, configFilesLocations)
         except MissingConfigOnServer:
+            post_box_status(False)
             sys.exit(1)
         try:
-            save_config(remoteConfig)
+            save_config(remoteConfig, envPath)
         except UnableToWriteConfig:
             sys.exit(1)
         reboot()
+        
+    try:
+        branch = get_config_key('BRANCH', currentConfig):  
+    except MissingKeyInConfig as e:
+        post_box_status(True, update_running=False, update_message=e)
+        sys.exit(1)
+
+    try:
+        check_branch_exist(branch, repo)
+    except BranchDoesNotExist as e:
+        post_box_status(True, update_running=False, update_message=e)
+                
+    try:
+        git_fetch(repo)
+    except UpdateFetchingError as e:
+        post_box_status(True, update_running=False, update_message=e)
+        sys.exit(1)
+        
+    try:
+        commitHash = get_config_key('COMMIT', currentConfig)
+    except MissingKeyInConfig:
+        commitHash = get_latest_commit(branch, repo)
+
+    try:
+        apply_commit(commitHash, repo)
+    except ApplyCommitError as e:
+        post_box_status(False, update_running=False, update_message=e)
+        sys.exit(1)
+    except ApplySameCommitException:
+        sys.exit(0)
+
+    try:
+        put_box_version(commitHash)
+    except PutVersionError as e:
+        post_box_status(False, update_running=False, update_message=e)
+        sys.exit(1)
+    reboot()
+
+
+    
