a370df04a5ff32fc8f0bd0cae5b21dead89edfb9
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-17 18:37:26 +0100

    remove indentation to config write up.

--- write_card.sh
+++ write_card.sh
@@ -15,9 +15,9 @@ echo "Creating ssh file to allow ssh connection on first boot..."
 touch $MOUNT_POINT/ssh.txt
 echo "Activating ETH1 hardware interface"
 echo "dtparam=i2c_arm=on
-	dtparam=i2s=on
-	dtparam=spi=on
-	dtoverlay=enc28j60" >> $MOUNT_POINT/config.txt
+dtparam=i2s=on
+dtparam=spi=on
+dtoverlay=enc28j60" >> $MOUNT_POINT/config.txt
 echo "Unmounting boot partition..."
 umount $BOOT_PARTITION
 echo "Done."
