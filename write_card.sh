883d87bfc02b6858a75cb3909aa5884e7a905f58
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-17 16:34:04 +0100

    Aditionnal ETH hardware interface support configured at SD card setup.

--- write_card.sh
+++ write_card.sh
@@ -13,6 +13,11 @@ echo "Mounting boot partition on specified mount point..."
 mount $BOOT_PARTITION $MOUNT_POINT
 echo "Creating ssh file to allow ssh connection on first boot..."
 touch $MOUNT_POINT/ssh.txt
+echo "Activating ETH1 hardware interface"
+echo "dtparam=i2c_arm=on
+	dtparam=i2s=on
+	dtparam=spi=on
+	dtoverlay=enc28j60" >> $MOUNT_POINT/config.txt
 echo "Unmounting boot partition..."
 umount $BOOT_PARTITION
 echo "Done."
