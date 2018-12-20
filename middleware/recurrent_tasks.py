de4ab7acb00434bd65371d57c70533f6a823dd74
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-20 13:00:12 +0100

    recurrent tasks refactoring done.

--- middleware/recurrent_tasks.py
+++ middleware/recurrent_tasks.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3.7
-from dotenv import load_dotenv, main
-import fileinput
-import git
-import os
-from pathlib import Path
-import requests
-import subprocess
 import sys
 
+from config import apply_config, fetch_config, save_config
+from config import ApplyConfigError, FetchConfigError, SaveConfigError
 from crontab import apply_crontab
 from crontab import ApplyCrontabError
-from utils import sign, post_box_status, put_box_version
-import utils
+from update import run_update
+from update import RunUpdateError
+from utils import post_box_status, reboot
+
 
 homePath = '/home/pi'
 
@@ -41,152 +38,6 @@ configFilesLocations = {
     ]
 }
 
-
-class ApplyCommitError(Exception):
-    pass
-
-
-class ApplySameCommitException(Exception):
-    pass
-
-
-class BranchDoesNotExist(Exception):
-    pass
-
-
-class FetchConfigError(Exception):
-    pass
-
-
-class FetchEstablishmentError(Exception):
-    pass
-
-
-class HeadAlreadyExists(Exception):
-    pass
-
-
-class MissingConfigOnServer(Exception):
-    pass
-
-
-class MissingKeyInConfig(Exception):
-    pass
-
-
-class RunningUpdateError(Exception):
-    pass
-
-
-class UpdateFetchingError(Exception):
-    pass
-
-
-class WriteConfigError(Exception):
-    pass
-
-def get_config_key(config, key):
-    try:
-        value = config[key]
-    except KeyError as key:
-        raise MissingKeyInConfig(f'{key} key is missing in configuration.')
-    return value
-
-
-def replace_occurences(key, value, fileLocation):
-    with fileinput.FileInput(fileLocation, inplace=True) as file:
-        for line in file:
-            print(line.replace(f'WC_{key}', value), end='')
-
-
-def get_current_config(configPath):
-    return main.dotenv_values(configPath)
-
-
-def copy_default_config(fromDir, toDir):
-    subprocess.call(f'cp -R {fromDir}/* {toDir}/', shell=True)
-
-
-def reboot():
-    subprocess.call('/sbin/shutdown -r now', shell=True)
-
-
-def save_config(config, configPath):
-    try:
-        with open(configPath, 'w') as file:
-            for key, value in config.items():
-                file.write(f'{key}="{value}"\n')
-    except Exception:
-        raise UnableToWriteConfig()
-
-
-def apply_config(config, configFiles):
-    for var in configFiles:
-        if var not in config:
-            raise MissingConfigOnServer()
-
-    for varName, fileLocations in configFiles.items():
-        for fileLocation in fileLocations:
-            replace_occurences(varName, config[varName], fileLocation)
-
-
-def get_remote_config():
-    keysPath = Path('/home/pi')
-    load_dotenv(dotenv_path=keysPath / 'keys', override=True)
-    API_KEY = os.environ['API_KEY']
-    API_SECRET = os.environ['API_SECRET']
-    apiHost = 'api.way-connect.com'
-
-    signature = sign(API_KEY, API_SECRET, {})
-    headers = {}
-    headers['Host'] = apiHost
-    headers['X-API-Key'] = API_KEY
-    headers['X-API-Sign'] = signature
-
-    try:
-        remoteConfig = requests.get(
-            url=f'https://{apiHost}/boxes/config/', headers=headers
-        )
-    except Exception as e:
-        raise FetchConfigError(e)
-
-    response = remoteConfig.json()
-
-    remoteHost = response['API_HOST']
-    try:
-        establishmentInfo = requests.get(
-            url=f'http://{remoteHost}/customers/establishment/',
-            headers=headers
-        )
-    except Exception as e:
-        raise FetchEstablishmentError(e)
-
-    response['ESTABLISHMENT_NAME'] = establishmentInfo.json()['name']
-    response['API_KEY'] = API_KEY
-    response['API_SECRET'] = API_SECRET
-    response['NGROK_SUBDOMAIN'] = API_KEY[:8]
-    return response
-
-
-def fetch_config(envPath):
-    print('Retrieving current config...', end='')
-    try:
-        currentConfig = get_current_config(envPath)
-    except GetCurrentConfigError as e:
-        print('FAIL')
-        raise FetchConfigError(e)
-    print('OK')
-
-    print('Retrieving remote config...', end='')
-    try:
-        remoteConfig = get_remote_config()
-    except GetRemoteConfigError as e:
-        print('FAIL')
-        raise FetchConfigError(e)
-    print('OK')
-    return currentConfig, remoteConfig
-
-
 if __name__ == '__main__':
     envFile = f'{homePath}/env'
     repoPath = f'{homePath}/Way-Connect_Box'
@@ -204,7 +55,7 @@ if __name__ == '__main__':
     print('----------------Run Update----------------')
     try:
         updateStatus = run_update(repoPath, remoteConfig)
-    except RunningUpdateError as e:
+    except RunUpdateError as e:
         post_box_status(update_running=False, update_message=str(e))
         sys.exit(1)
     print('------------------------------------------')
@@ -222,7 +73,7 @@ if __name__ == '__main__':
         try:
             apply_crontab(remoteConfig, cronFile)
         except ApplyCrontabError as e:
-            post_box_status(crontab_running=False, crontab_running=str(e))
+            post_box_status(crontab_running=False, crontab_message=str(e))
             sys.exit(1)
         print('------------------------------------------')
 
