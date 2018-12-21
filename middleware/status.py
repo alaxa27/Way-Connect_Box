13d097891b9a7bbcccb0619e9df76ff834efdd3a
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-21 15:10:36 +0100

    statuses related methods in own module

---
+++ middleware/status.py
@@ -0,0 +1,139 @@
+import requests
+import traceback
+import subprocess
+import sys
+
+
+class RetrieveServiceStatusError(Exception):
+    pass
+
+
+class ServiceStateError(Exception):
+    pass
+
+
+def post_box_status(
+    dhcpd_running=True,
+    dhcpd_message='Default message.',
+    dnsmasq_running=True,
+    dnsmasq_message='Default message.',
+    hostapd_running=True,
+    hostapd_message='Default message.',
+    internet_connection_active=True,
+    internet_connection_message='Default message.',
+    nodogsplash_running=True,
+    nodogsplash_message='Default message.',
+    update_running=True,
+    update_message='Default message.'
+):
+
+    boxStatus = {}
+
+    boxStatus['dhcpd_running'] = dhcpd_running
+    boxStatus['dhcpd_message'] = dhcpd_message
+    boxStatus['dnsmasq_running'] = dnsmasq_running
+    boxStatus['dnsmasq_message'] = dnsmasq_message
+    boxStatus['hostapd_running'] = hostapd_running
+    boxStatus['hostapd_message'] = hostapd_message
+    boxStatus['internet_connection_active'] = internet_connection_active
+    boxStatus['internet_connection_message'] = internet_connection_message
+    boxStatus['nodogsplash_running'] = nodogsplash_running
+    boxStatus['nodogsplash_message'] = nodogsplash_message
+    boxStatus['update_running'] = update_running
+    boxStatus['update_message'] = update_message
+    boxStatus['connected_customers'] = 0
+
+    requests.post(
+        url='http://localhost:5000/portal/boxes/status/',
+        json=boxStatus
+    )
+
+
+def post_error_status(type):
+    error = {}
+    error['error_type'] = type
+    error['error_traceback'] = traceback.format_exc()
+    post_box_status(
+        internet_connection_active=False,
+        internet_connection_message=error
+        )
+    traceback.print_exc()
+    sys.exit(1)
+
+
+def post_service_status():
+    services = [
+        {
+            'from': 'middleware',
+            'to': 'dhcpd'
+        },
+        {
+            'from': 'nginx',
+            'to': 'dnsmasq'
+        },
+        {
+            'from': 'hostapd',
+            'to': 'hostapd',
+        },
+        {
+            'from': 'nodogsplash',
+            'to': 'nodogsplash'
+        },
+        {
+            'from': 'ngrok',
+            'to': 'update'
+        },
+    ]
+
+    request = {}
+    for service in services:
+        status_dict = retrieve_service_status(service['from'])
+        status_state = service_state(status_dict)
+        if status_state:
+            status_dict = {}
+        request[service['to']] = {
+            'running': status_state,
+            'message': str(status_dict)
+        }
+    # Retrieve services status
+    post_box_status(
+        dhcpd_running=request['dhcpd']['running'],
+        dhcpd_message=request['dhcpd']['message'],
+        dnsmasq_running=request['dnsmasq']['running'],
+        dnsmasq_message=request['dnsmasq']['message'],
+        hostapd_running=request['hostapd']['running'],
+        hostapd_message=request['hostapd']['message'],
+        nodogsplash_running=request['nodogsplash']['running'],
+        nodogsplash_message=request['nodogsplash']['message'],
+        update_running=request['update']['running'],
+        update_message=request['update']['message'],
+    )
+
+
+def retrieve_service_status(serviceName):
+    try:
+        key_value = subprocess.check_output(
+            ['/bin/systemctl', 'show', serviceName],
+            universal_newlines=True).split('\n')
+    except subprocess.SubprocessError:
+        raise RetrieveServiceStatusError()
+    except OSError:
+        raise RetrieveServiceStatusError()
+
+    json_dict = {}
+    for entry in key_value:
+        kv = entry.split("=", 1)
+        if len(kv) == 2:
+            json_dict[kv[0]] = kv[1]
+
+    return json_dict
+
+
+def service_state(statusDict):
+    try:
+        activeState = statusDict['ActiveState']
+    except KeyError:
+        raise ServiceStateError()
+    if activeState == 'active':
+        return True
+    return False
---
+++ middleware/tests/test_status.py
@@ -0,0 +1,96 @@
+import traceback
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+import status
+
+
+class TestStatus(TestCase):
+    @patch('traceback.print_exc')
+    @patch('sys.exit')
+    @patch('status.post_box_status')
+    def test_post_error_status(self, postMock, _, printExcMock):
+        """should return the correct traceback and type"""
+        type = 'testType'
+        try:
+            try:
+                raise Exception('A')
+            except Exception:
+                raise Exception('B')
+        except Exception:
+            tb = traceback.format_exc()
+            status.post_error_status(type)
+
+        expectedMessage = {
+            'error_type': type,
+            'error_traceback': str(tb)
+        }
+
+        postMock.assert_called_with(
+            internet_connection_active=False,
+            internet_connection_message=expectedMessage
+            )
+        printExcMock.assert_called()
+
+    @patch('subprocess.check_output')
+    def test_retrieve_service_status(self, checkOutputMock):
+        checkOutputMock.return_value = 'A=foo\nB=bar\n'
+        expectedResult = {'A': 'foo', 'B': 'bar'}
+        result = status.retrieve_service_status('test')
+
+        self.assertEqual(result, expectedResult)
+
+    @patch('status.post_box_status')
+    def test_post_service_status(
+        state,
+        postBoxStatusMock
+    ):
+        def retrieve_service_status_side_effect(serviceName):
+            if serviceName == 'ngrok':
+                return 'failed'
+            return 'tst'
+
+        def service_state_side_effect(statusDict):
+            if statusDict == 'failed':
+                return False
+            return True
+
+        serviceStateMock = MagicMock(
+            name='status.service_state',
+            side_effect=service_state_side_effect
+            )
+        retrieveServiceStatusMock = MagicMock(
+            name='status.retrieve_service_status',
+            side_effect=retrieve_service_status_side_effect
+            )
+        with patch('status.retrieve_service_status', retrieveServiceStatusMock),\
+                patch('status.service_state', serviceStateMock):
+            status.post_service_status()
+
+        postBoxStatusMock.assert_called_with(
+            dhcpd_running=True,
+            dhcpd_message='{}',
+            dnsmasq_running=True,
+            dnsmasq_message='{}',
+            hostapd_running=True,
+            hostapd_message='{}',
+            nodogsplash_running=True,
+            nodogsplash_message='{}',
+            update_running=False,
+            update_message='failed',
+            )
+
+    def test_service_state(self):
+        result = status.service_state({
+            'ActiveState': 'active'
+        }) 
+        expectedResult = True
+
+        self.assertEqual(result, expectedResult)
+
+        result = status.service_state({
+            'ActiveState': 'foo'
+        }) 
+        expectedResult = False
+
+        self.assertEqual(result, expectedResult)
