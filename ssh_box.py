e7a56e72bb161fab21788724f317c33150084c5e
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-12-24 03:40:42 +0100

    script to connect directly to Way-Boxes

---
+++ ssh_box.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import requests
+import subprocess
+
+
+parser = argparse.ArgumentParser(description='Connects to Way-Box through ssh.')
+parser.add_argument(
+        '-s',
+        '--server',
+        type=str,
+        default='tunnel.way-connect.com',
+        help='ngrokd domain name'
+        )
+
+parser.add_argument(
+        '-u',
+        '--user',
+        type=str,
+        help='the user on the remote',
+        default='pi'
+        )
+
+parser.add_argument(
+        '-k',
+        '--key',
+        type=str,
+        help='the API key of the Way-Box',
+        required=True
+        )
+
+parser.add_argument(
+        '-c',
+        '--connect',
+        action='store_true',
+        help='connects automatically'
+        )
+
+parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='explain what is being done'
+        )
+
+args = vars(parser.parse_args())
+
+
+def get_port_from_url(url):
+    return url.split(':')[-1]
+
+
+def ssh_connect(url, user=None, port=22):
+    if user:
+        url = f'{user}@{url}'
+
+    command = ['ssh', url, '-p', port] 
+    command = ['/bin/sh', '-c', '"ssh {url} -p {port}"']
+
+    subprocess.run(
+        command,
+        shell=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False
+        )
+
+
+if __name__=='__main__':
+    key = args['key']
+    serverUrl = args['server']
+
+    resp = requests.get(f'http://{key}.{serverUrl}:8000/api/tunnels')
+
+    data = resp.json() #json.loads(resp.json)
+    tunnels = data['Tunnels']
+    if args['verbose']:
+        print(json.dumps(tunnels, indent=4, sort_keys=True))
+    for tunnel in tunnels:
+        port = get_port_from_url(tunnel['LocalAddr'])
+        if port == str(22):
+            sshPort = get_port_from_url(tunnel['PublicUrl'])
+            url = f'{key}.{serverUrl}'
+
+            print(f'ssh {args["user"]}@{url} -p {sshPort}')
+            if args['verbose']:
+                print(f'Port: {sshPort}')
+                print(f'URL: {url}')
+            if args['connect']:
+                ssh_connect(url, user=args['user'], port=sshPort)
+
